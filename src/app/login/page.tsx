08444373b3d7a26a80f07fa02de5e55e4988a676
Euri <euripidec@gmail.com>
2023-08-12 22:01:32 -0400

    Added new designs for the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,32 +39,55 @@ export default function LoginPage() {
     })
     return (
 
-        <div className="  flex flex-col items-center justify-center 
-        min-h-screen py-2 ">
-            <h1>{loading ?"Procesando" : "Ingresa"}</h1>
-            <hr/>
-            
-            <label htmlFor="Correro Electronico">Email</label>
-            <input 
-            className="p-2 border border-grau-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-                id="Email"
-                type="text"
-                value={user.Email}
-                onChange={(e) => setUser({...user, Email: e.target.value})}
-                />
-            <label htmlFor="Contrasena">Password</label>
-            <input 
-            className="p-2 border border-grau-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-                id="Password"
-                type="password"
-                value={user.Password}
-                onChange={(e) => setUser({...user, Password: e.target.value})}
-                />
-            <button 
-            onClick={onLogin}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none
-            focus:border-gray-600">{buttonDisabled ? "Por favor complete la información necesaria" : "Ingresa"}</button>
-            <Link href="/signup">Registrase</Link>
+        <section className='flex flex-col md:flex-row h-screen items-center'>
+
+        <div className='h-screen flex justify-center items-center md:w-1/2 xl:w-2/3'>
+  
+        <div className='flex justify-center items-center absolute'>
+            <h3 className=' text-[10px] font-light text-center text-white md:leading-[55px] sm:leading-[55px] sm:text-5xl'>SOMOS EL RENACER DE LA ESPERANZA DEL<br /> PUEBLO. LA EXPRESIÓN DE LA VOLUNTAD<br /> CIUDADANA POR LA RENOVACIÓN DE LA<br /> DEMOCRACIA EN LA REPÚBLICA DOMINICANA.</h3>
         </div>
+          <img src="/images/image.png" className='w-full h-full object-cover' alt='imagen Faro de Colon' />
+         
+        </div>
+  
+        <div className='bg-white items-center justify-center flex md-mx-auto lg:max-w-md md:max-w-md md:w-1/2 xl:w-1/3 px-6 lg:px-16 xl:px-12'>
+          <div className='w-full h-100'>
+            <h1 className='text-2xl  font-semibold '> SDEngage</h1>
+            <h2 className='text-xl md:text-2xl font-bold leading-tight mt-12'>Inicia sesión en tu cuenta</h2>
+  
+            <form action='#' method='POST' className='mt-6'>
+              <div>
+                <label className='block text-gray-700'>Correo Electrónico</label>
+                <input type='email' id='email' placeholder='Ingresa Correo Electrónico' className='w-full bg-gray-200 rounded-lg px-4 py-3 mt-2 border focus:border-[#14A647] focus:outline-none focus:bg-white'  value={user.Email}
+                onChange={(e) => setUser({...user, Email: e.target.value})} autoComplete='email' autoFocus required /> 
+              </div>
+  
+              <div className='mt-4'>
+                <label className='block text-gray-700'>Contraseña</label>
+                <input type='password' id='password' minLength={6} placeholder='Ingresa tu contraseña' className='w-full bg-gray-200 rounded-lg px-4 py-3 mt-2 border focus:border-[#14A647] focus:outline-none focus:bg-white' value={user.Password}
+                onChange={(e) => setUser({...user, Password: e.target.value})} autoComplete='current-password' autoFocus required /> 
+              </div>
+              
+              <div className='text-right mt-2'>
+                <a href='#' type='submit' className='text-sm font-semibold text-gray-700 hover:text-[#14A647] focus:text-blue-700 focus:outline-none'>¿Olvidaste la contraseña?</a>
+              </div>
+  
+              <button type='submit' className='w-full block bg-[#14A647]  hover:bg-[#0A732F] px-4 py-3 mt-6 rounded-lg font-semibold text-white focus:bg-blue-400 focus:outline-none'  onClick={onLogin}>Iniciar sesión</button>
+  
+              <p className='mt-8'>
+                ¿Necesitas una cuenta?
+                <a href='/signup' className='text-[#14A647] hover:text-[#0A732F] font-semibold'> Crear una cuenta</a>
+              </p>
+  
+              <p className='text-sm text-gray-500 mt-12'>
+                &copy; 2023 SDEngage inicio de sesión
+              </p>
+  
+            </form>
+  
+          </div>
+        </div>
+  
+      </section>
     )
 }
