86192abd5dba7d84c0e9b0e52cd15ff8b3be7d2a
Euri <euripidec@gmail.com>
2023-08-17 22:47:30 -0400

    test to see if page version of Next js works

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-between gap-2 px-2 sm:px-6 m-[34px_0_12px] sm:m-[24px_0_24px]">
+      <p className="text-[var(--color-gray-5)] dark:text-white">
+        Copyright © 2023 SDE💖Despierta, All rights Reserved
+      </p>
+      <p className="text-[var(--color-gray-5)] dark:text-white">
+        Design & Developed by{" Euripide Carpio "}
+        <Link href="/" className="text-[var(--color-primary-dark)]">
+          {" "}
+          Ivans Development Team
+        </Link>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/layout.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+
+
+import { useRouter, usePathname } from "next/navigation";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import Footer from './footer/Footer';
+import NavBar from './navBar/NavBar';
+import SideBar from './sideBar/SideBar';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [showText, setShowText] = useState(true);
+  const [openSidBar, setOpenSidBar] = useState(false);
+
+  const responsive = useMediaQuery({
+    query: "(max-width: 1200px)",
+  });
+
+  const pathname = usePathname();
+
+  const clss = pathname === "/profile" ? "" : "mx-2 sm:mx-6";
+
+  return (
+    <>
+      
+
+      <div className="flex items-start">
+        {/* Side Bar */}
+        <SideBar
+          showText={showText}
+          setShowText={setShowText}
+          openSidBar={openSidBar}
+          setOpenSidBar={setOpenSidBar}
+        />
+
+        <div
+          className={`w-full flex-1 pl-0 ${
+            showText
+              ? responsive
+                ? "lg:pl-[212px]"
+                : "lg:pl-[312px]"
+              : "lg:pl-[150px]"
+          } transition-all duration-500 ease-in-out`}
+        >
+          {/* Nav Bar */}
+          <NavBar
+            setIsOpen={setIsOpen}
+            isOpen={isOpen}
+            openSidBar={openSidBar}
+            setOpenSidBar={setOpenSidBar}
+          />
+
+          <section
+            className={`flex flex-col xl:flex-row gap-5 ${clss} mt-5 sm:mt-10`}
+          >
+            {children}
+          </section>
+
+          {/* Footer section */}
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
