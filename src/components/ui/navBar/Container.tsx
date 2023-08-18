71ce2f8a1e76143e568695564011d0ca35764f6a
Morphy <20209904@itla.edu.do>
2023-08-18 13:17:04 -0400

    .

--- src/components/ui/navBar/Container.tsx
+++ src/components/ui/navBar/Container.tsx
@@ -4,6 +4,14 @@ import React from 'react';
 import { useState } from 'react';
 
 
+import {
+
+  Bars3Icon,
+
+} from '@heroicons/react/24/outline'
+
+
+
 
 export default function Container({ children }: { children: React.ReactNode }) {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -14,13 +22,24 @@ export default function Container({ children }: { children: React.ReactNode }) {
 		<nav
 			className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
 			aria-label="Global">
+                
 			{children}
+            <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
 
 			{/*peper.Group */}
-        
+            
 
             {/*Dialog*/}
-           
-            	</nav>
+            
+     	</nav>
 	);
 }
